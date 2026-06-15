47497f6f08fba6af5d89d37b9e03ea6c561119ae
Matheus Carneiro <carneiromatheus943@gmail.com>
2026-06-15 10:35:30 -0300

    added am/pm check

--- digitalwatch/watch_script.js
+++ digitalwatch/watch_script.js
@@ -1,22 +1,28 @@
+//
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
+const ampmElement = document.getElementById('ampm');
 
 const watch = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
+    let ampm;
+
+    if (hr <= 12) {
+        ampm = "AM";
+    } else {
+        ampm = "PM";
+    }
 
     if (hr < 10) hr = '0' + hr;
-
-
     if (min < 10) min = '0' + min;
-
-
     if (s < 10) s = '0' + s;
 
     hours.textContent = hr;
     minutes.textContent = min;
     seconds.textContent = s;
-})
+    ampmElement.textContent = ampm;
+}, 1000);
